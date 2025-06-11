88dca6568e468974c94b4ba5983bdbd3eb8a932b
Florencio <florencio.pool@hotmail.com>
2025-06-11 16:35:51 -0600

    add entity

--- src/main/java/com/RESTapp/restapp/entity/User.java
+++ src/main/java/com/RESTapp/restapp/entity/User.java
@@ -1,4 +1,17 @@
 package com.RESTapp.restapp.entity;
 
+import jakarta.persistence.*;
+
+//Java POJO class
+@Entity
+@Table(name="user")
 public class User {
+
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+
 }
